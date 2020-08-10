b6f23e9d1ae54bbefc954625e599dad4d9bacbc7
maleich <mleich@athenian.org>
2020-08-10 14:53:31 -0700

    remove test code

--- my_code.py
+++ my_code.py
@@ -1,16 +1,23 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
+#
 
-def inc(x):
-    # User code goes here
-    return x + 1
+'''
+The line at the top a comment.
+This is also a comment - this version is used if you have a few lines of explanation.
+Use comments to document what is happening in your programs and to leave notes for others.
+'''
+
+# The print() function
 
 
-if __name__ == '__main__':
-    # Test your code with this first
-    # Change the argument to try different values
-    print(inc(6))
 
-    # After you are satisfied with your results, use input() to prompt the user for a value:
-    #v = input("Value to increment: ")
-    #print(inc(int(v)))
+# Hello world
+
+
+
+
+# Variables
+
+
+
+# input()
