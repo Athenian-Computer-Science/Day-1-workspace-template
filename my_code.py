a3e5f9b93150ae877ee2646c556ff3f1521e673b
Megan Leich <51799170+maleich@users.noreply.github.com>
2020-08-29 21:38:00 -0700

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -1,8 +1,8 @@
-# Collaborators (including web sites where you got help: (enter none if you didn't need help)
+# Collaborators, including web sites where you got help: (enter none if you didn't need help)
 #
 
 '''
-The line at the top a comment.
+The line at the top is a comment.
 This is also a comment - this version is used if you have a few lines of explanation.
 Use comments to document what is happening in your programs and to leave notes for others.
 '''
